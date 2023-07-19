c0112e681dace75a22c9614ec6e3cd51cfd1b98f
Kiwifuit <mahkiwi123@gmail.com>
2023-07-19 21:48:05 +0800

    Batch downloads fr

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,12 +1,21 @@
 use futures_util::StreamExt;
-use reqwest::get;
+use rayon::ThreadPool;
+use rayon::ThreadPoolBuilder;
+use rayon::{prelude::*, scope};
 use reqwest::header;
+use reqwest::Response;
 
 use std::fs::OpenOptions;
 use std::io::{self, prelude::*};
 
 pub trait Downloadable {
-    fn url(&self) -> String;
+    fn urls(&self) -> Option<Vec<String>> {
+        None
+    }
+
+    fn url(&self) -> Option<String> {
+        None
+    }
 
     fn filename(&self) -> Option<String> {
         None
@@ -14,57 +23,85 @@ pub trait Downloadable {
 }
 
 impl Downloadable for String {
-    fn url(&self) -> String {
-        self.to_owned()
+    fn url(&self) -> Option<String> {
+        Some(self.to_owned())
+    }
+}
+
+impl Downloadable for &String {
+    fn url(&self) -> Option<String> {
+        Some(String::from(*self))
     }
 }
 
 pub enum DownloadError {
     NetworkError(reqwest::Error),
     CreateFileError(io::Error),
+    NothingToDownload,
 }
 
-pub async fn download_image<I, O>(image: &I) -> Result<usize, DownloadError>
+pub async fn download_image<'a, I>(image: &'a I) -> Result<usize, DownloadError>
 where
-    I: Downloadable,
+    I: Downloadable + 'a,
+    &'a I: Downloadable,
 {
-    let resp = get(image.url())
-        .await
-        .map_err(|e| DownloadError::NetworkError(e))?;
-    let headers = resp.headers().clone();
-    let mut stream = resp.bytes_stream();
-
-    let filename = image.filename().unwrap_or_else(|| {
-        let raw = headers
-            .get(header::CONTENT_DISPOSITION)
-            .unwrap()
-            .to_str()
-            .unwrap();
-
-        let length = raw.len();
-        let index = raw
-            .find("=")
-            .expect("expected to find an '=' somewhere in the disposition");
-        let disposition = &raw[index + 1..length - 1];
-
-        disposition.to_string()
-    });
-
-    let mut savefile = OpenOptions::new()
-        .create_new(true)
-        .write(true)
-        .open(filename)
-        .map_err(|e| DownloadError::CreateFileError(e))?;
-
+    let resps = get(image).await?;
     let mut downloaded_total = 0;
-    while let Some(chunk) = stream.next().await {
-        let bytes = chunk.unwrap();
-        downloaded_total += bytes.len();
 
-        savefile
-            .write(&bytes)
-            .expect("expected for chunk to be written");
+    for resp in resps {
+        let headers = resp.headers().clone();
+        let mut stream = resp.bytes_stream();
+
+        let filename = image.filename().unwrap_or_else(|| {
+            let raw = headers
+                .get(header::CONTENT_DISPOSITION)
+                .unwrap()
+                .to_str()
+                .unwrap();
+
+            let length = raw.len();
+            let index = raw
+                .find("=")
+                .expect("expected to find an '=' somewhere in the disposition");
+            let disposition = &raw[index + 1..length - 1];
+
+            disposition.to_string()
+        });
+
+        let mut savefile = OpenOptions::new()
+            .create_new(true)
+            .write(true)
+            .open(filename)
+            .map_err(|e| DownloadError::CreateFileError(e))?;
+
+        while let Some(chunk) = stream.next().await {
+            let bytes = chunk.unwrap();
+            downloaded_total += bytes.len();
+
+            savefile
+                .write(&bytes)
+                .expect("expected for chunk to be written");
+        }
     }
 
     Ok(downloaded_total)
+}
+
+async fn get<D: Downloadable>(url: D) -> Result<Vec<Response>, DownloadError> {
+    if let Some(link) = url.url() {
+        Ok(vec![reqwest::get(link)
+            .await
+            .map_err(|e| DownloadError::NetworkError(e))?])
+    } else if let Some(links) = url.urls() {
+        let resps = links.iter().map(|i| reqwest::get(i));
+
+        let mut res = vec![];
+        for resp in resps {
+            res.push(resp.await.map_err(|e| DownloadError::NetworkError(e))?)
+        }
+
+        Ok(res)
+    } else {
+        Err(DownloadError::NothingToDownload)
+    }
 }
