4992cd23920d2a1e6fac8926bb4ff47fcf504a13
Kiwifuit <mahkiwi123@gmail.com>
2023-08-17 00:09:22 +0800

    First Run Message

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ mod types;
 
 #[tokio::main]
 async fn main() {
+    // TODO: Replace with INFO logs
+    println!("========================================================");
+    println!("First Run!");
+    println!("Resources will be indexed and downloaded for faster");
+    println!("startup times in the future");
+    println!("");
+    println!("This WILL take a while, and will download around 100");
+    println!("megabytes worth of images");
+    println!("========================================================");
+
     let (resources, characters) = index::index_characters().await;
 
     println!("Indexed {} characters", characters.len());
@@ -12,5 +22,5 @@ async fn main() {
         println!("{}", character);
     }
 
-    dbg!(resources);
+    println!("{} resource(s) to be downloaded", resources.len());
 }
