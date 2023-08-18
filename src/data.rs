11956113a5ebf3edb155ebf0ae28a8c29f0526d8
Kiwifuit <mahkiwi123@gmail.com>
2023-08-18 22:13:20 +0800

    Forgot that the directory has to be created if it doesnt exist Whoops!

--- src/data.rs
+++ src/data.rs
@@ -1,9 +1,13 @@
 use crate::types::*;
-use std::fs::OpenOptions;
+use std::fs::{create_dir_all, OpenOptions};
 
 pub fn write_character(character: &Character) {
-    let filename = herta::data::get_root_dir(env!("CARGO_BIN_NAME"), Some("characters"))
-        .join(format!("{}.json", character.name()));
+    let root_dir = herta::data::get_root_dir(env!("CARGO_BIN_NAME"), Some("characters"));
+    if !root_dir.exists() {
+        create_dir_all(&root_dir);
+    }
+
+    let filename = root_dir.join(format!("{}.json", character.name()));
     let mut file = OpenOptions::new().write(true).open(filename).unwrap();
 
     herta::data::write_config(&mut file, character);
