ba3a4f2c235df3c3a6229441781766fd55ea0ea5
Kiwifuit <mahkiwi123@gmail.com>
2024-02-23 14:19:48 +0800

    Added warning for first run

--- src/App.js
+++ src/App.js
@@ -7,23 +7,30 @@ import { listen } from "@tauri-apps/api/event";
 function App() {
   return (
     <div className="app" >
-      <img className="herta-logo" src={logo} alt="Project Herta Logo" />
+      <img onLoad={() => { invoke("begin_first_run") }} className="herta-logo" src={logo} alt="Project Herta Logo" />
       <h1>Herta needs to get stuff!</h1>
-      <p>Miss Herta needs to collect information before everything starts</p>
-      <progress id="first-run" max="10" value="0"></progress>
+      <p>Miss Herta needs to collect information before everything starts.</p>
+      <p className="warn">This will take ~10 minutes, but will never happen again</p>
+      <progress id="first-run" value="0"></progress>
       <p id="first-run-label"></p>
-      <button onClick={() => invoke("test_progress")}>Click Me!</button>
     </div>
   );
 }
 
 listen("download-progress", (e) => {
-  let { progress, message } = e.payload;
+  let { current_progress, message } = e.payload;
   let progress_bar = document.getElementById("first-run");
   let progress_label = document.getElementById("first-run-label");
 
-  progress_bar.value += progress;
-  progress_label.innerText = `Currently on: ${message};`
+  progress_bar.value = current_progress;
+  progress_label.innerText = message
+})
+
+listen("start-progress", (e) => {
+  let { total } = e.payload;
+  let progress_bar = document.getElementById("first-run");
+
+  progress_bar.setAttribute("max", `${total}`);
 })
 
 export default App;
